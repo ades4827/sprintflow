25ca6c8dc0dbb0a22c9bc33d2feec3132b39a1bd
Michele Lucchina <michele@widesrl.com>
2024-01-30 15:52:39 +0100

    update datatable blades

--- resources/views/datatable/search-with-export.blade.php
+++ resources/views/datatable/search-with-export.blade.php
@@ -1,4 +1,7 @@
 <div class="input-group flex-1">
     <input type="text" name="filter" class="form-control" placeholder="Cerca" autocomplete="off">
     <button class="btn btn-primary btn-search input-group-text">Cerca</button>
+    @can($export_data['permission'])
+        <button type="submit" class="btn btn-primary btn-export input-group-text">Export</button>
+    @endcan
 </div>
