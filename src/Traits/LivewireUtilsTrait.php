493ef3406ef5de71ea0e949db00824eadd920d61
Michele Lucchina <michele@lucchina.com>
2024-04-28 12:53:42 +0200

    fix for livewire 3

--- src/Traits/LivewireUtilsTrait.php
+++ src/Traits/LivewireUtilsTrait.php
@@ -32,16 +32,13 @@ trait LivewireUtilsTrait
                 $type = $options['type'];
             }
 
-            $this->dispatchBrowserEvent('livewire-alert', ['type' => $type,  'title' => '',  'message' => $message]);
+            $this->dispatch('livewire-alert', type: $type, title: '', message: $message);
 
             if (! in_array('notCloseModal', $options)) {
-                $this->emit('closeModal');
-            }
-            if (isset($options['calendar_to_refresh'])) {
-                $this->dispatchBrowserEvent('calendar_to_refresh', ['room_id' => $options['calendar_to_refresh']]);
+                $this->dispatch('closeModal');
             }
             if (isset($options['table_to_refresh'])) {
-                $this->dispatchBrowserEvent('eventRefresh-datatable', ['table_selector' => $options['table_to_refresh']]);
+                $this->dispatch('eventRefresh-datatable', table_selector: $options['table_to_refresh']);
             } else {
                 dd('Inserisci il valore table_to_refresh');
             }
@@ -60,50 +57,5 @@ trait LivewireUtilsTrait
         }
 
         return redirect()->route($route_name, $route_parameters);
-    }
-
-    /**
-     * Returns a file size limit in bytes based on the PHP upload_max_filesize and post_max_size
-     *
-     * @return int
-     */
-    protected function file_upload_max_size()
-    {
-        static $max_size = -1;
-
-        if ($max_size < 0) {
-            // Start with post_max_size.
-            $post_max_size = $this->parse_size(ini_get('post_max_size'));
-            if ($post_max_size > 0) {
-                $max_size = $post_max_size;
-            }
-
-            // If upload_max_size is less, then reduce. Except if upload_max_size is
-            // zero, which indicates no limit.
-            $upload_max = $this->parse_size(ini_get('upload_max_filesize'));
-            if ($upload_max > 0 && $upload_max < $max_size) {
-                $max_size = $upload_max;
-            }
-        }
-
-        return $max_size;
-    }
-
-    /**
-     * Parse file size
-     *
-     * @param  $size
-     * @return float
-     */
-    private function parse_size($size)
-    {
-        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
-        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
-        if ($unit) {
-            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
-            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
-        }
-
-        return round($size);
     }
 }
