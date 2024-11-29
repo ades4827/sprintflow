a973e3a36b4cfe06e8e2c759f31761d36e9e8a6f
Michele Lucchina <michele.lucchina@jokersrl.it>
2024-11-29 15:04:04 +0100

    fa icon free

--- resources/views/datatable/actions/simple-crud-modal-edit.blade.php
+++ resources/views/datatable/actions/simple-crud-modal-edit.blade.php
@@ -4,14 +4,14 @@
             <li>
                 <a onclick='Livewire.dispatch("openModal", { component: "modal-edit", arguments: {"model": "{{ $model->getClassSlug() }}", "model_id": {{$model->id}} }} )' class="btn btn-primary btn-icon-sm tooltip"
                    data-placement="bottom" title="{{ __('sprintflow::view.edit') }}">
-                    <i class="fa-light fa-pen-to-square"></i></a>
+                    <i class="fa-solid fa-pen-to-square"></i></a>
             </li>
         @endcan
         @can($model->getPermissionPrefix().'.delete')
             <li>
                 <a href="{{ route('admin.'.$model->getClassSlug(true).'.delete', [$model->getClassSlug() => $model->id]) }}" class="btn btn-danger btn-icon-sm with-confirm tooltip"
                    data-placement="bottom" title="{{ __('sprintflow::view.delete') }}">
-                    <i class="fa-light fa-trash"></i></a>
+                    <i class="fa-solid fa-trash"></i></a>
             </li>
         @endcan
     @else
@@ -19,7 +19,7 @@
             <li>
                 <a href="{{ route('admin.'.$model->getClassSlug(true).'.restore', ['deleted_'.$model->getClassSlug() => $model->id]) }}" class="btn btn-warning btn-icon-sm tooltip"
                    data-placement="bottom" title="{{ __('sprintflow::view.restore') }}">
-                    <i class="fa-regular fa-trash-undo"></i></a>
+                    <i class="fa-solid fa-trash-can-arrow-up"></i></a>
             </li>
         @endcan
     @endif
--- resources/views/datatable/fields/generic-boolean.blade.php
+++ resources/views/datatable/fields/generic-boolean.blade.php
@@ -1,5 +1,5 @@
 @if( $value )
-    <i class="fa-light fa-check text-green"></i>
+    <i class="fa-solid fa-check text-green"></i>
 @else
     -
 @endif
