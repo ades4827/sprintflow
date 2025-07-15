dc94757ea564863640a25afc8a8306ac97d5ba32
Michele Lucchina <michele.lucchina@jokersrl.it>
2025-07-15 17:20:15 +0200

    datatable search wireui

---
+++ resources/views/datatable/search-wireui-with-export.blade.php
@@ -0,0 +1,11 @@
+<div class="input-group flex-1">
+    <x-input name="filter" placeholder="{{ __('sprintflow::view.search') }}" autocomplete="off">
+    <button class="btn btn-primary btn-search input-group-text">{{ __('sprintflow::view.search') }}</button>
+    @if(isset($export_data) && $export_data['permission'])
+        @can($export_data['permission'])
+            <button type="submit" class="btn btn-primary btn-export input-group-text">{{ __('sprintflow::view.export') }}</button>
+        @endcan
+    @else
+        <button type="submit" class="btn btn-primary btn-export input-group-text">{{ __('sprintflow::view.export') }}</button>
+    @endif
+</div>
---
+++ resources/views/datatable/search-wireui.blade.php
@@ -0,0 +1,4 @@
+<div class="input-group flex-1">
+    <x-input name="filter" placeholder="{{ __('sprintflow::view.search') }}" autocomplete="off" />
+    <button class="btn btn-primary btn-search input-group-text">{{ __('sprintflow::view.search') }}</button>
+</div>
