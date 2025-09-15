f40d61c9f902aeb8a3bc79890f08801580dd6334
Michele Lucchina <michele.lucchina@jokersrl.it>
2025-09-15 17:44:51 +0200

    form footer remove icon

--- resources/views/livewire/form-footer.blade.php
+++ resources/views/livewire/form-footer.blade.php
@@ -42,11 +42,11 @@
             @if( !isset($hide_submit) )
                 <button type="submit" class="btn btn-primary" wire:loading.attr="disabled">
                     <div class="block sm:hidden">
-                        <span wire:loading.remove><i class="fa-solid fa-floppy-disk"></i> {{ isset($submit_label) ? $submit_label : __('sprintflow::view.save') }}</span>
+                        <span wire:loading.remove>@if(!isset($submit_label))<i class="fa-solid fa-floppy-disk"></i>@endif {{ isset($submit_label) ? $submit_label : __('sprintflow::view.save') }}</span>
                         <i wire:loading class="fa-solid fa-spinner fa-spin-pulse"></i>
                     </div>
                     <div class="hidden sm:block">
-                        <i class="fa-solid fa-floppy-disk mr-1"></i>
+                        @if(!isset($submit_label))<i class="fa-solid fa-floppy-disk mr-1"></i>@endif
                         <span wire:loading.remove>{{ isset($submit_label) ? $submit_label : __('sprintflow::view.save') }}</span>
                         <span wire:loading>{{ __('sprintflow::view.wait') }}...</span>
                     </div>
