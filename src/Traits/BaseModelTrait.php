486f01182cdbe8dc2c686dd78d55459524760fda
Michele Lucchina <michele.lucchina@jokersrl.it>
2025-09-17 12:18:22 +0200

    reformat base model and routing

--- src/Traits/BaseModelTrait.php
+++ src/Traits/BaseModelTrait.php
@@ -6,9 +6,72 @@ use Illuminate\Support\Str;
 
 trait BaseModelTrait
 {
-    public static function getTableName(): string
+    /*
+    protected $hidden = [
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+    */
+
+    /*
+    protected $casts = [
+        'is_valid' => 'boolean',
+        'created_at' => 'datetime',
+        'datas' => 'object',
+    ];
+    */
+
+    /*
+    composer require spatie/laravel-translatable
+    use Spatie\Translatable\HasTranslations;
+    public array $translatable = [
+        'name'
+    ];
+    */
+
+    /*
+    public static function boot()
     {
-        return with(new static)->getTable();
+        static::addGlobalScope('valid', function (Builder $builder) {
+            $builder->where('is_valid', true);
+        });
+        parent::boot();
+    }
+    protected static function booted()
+    {
+        static::creating(static function ($model) {
+            if (empty($model->slug)) {
+                $slug = \Illuminate\Support\Str::slug($model->name);
+                //$slug = Str::slug($model->translate('name', 'en'));
+                $count = static::whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")->count();
+                $model->slug = $count ? "{$slug}-{$count}" : $slug;
+            }
+        });
+    }
+    */
+
+    protected $class_plural = null;
+
+    public function getClassPlural(): string
+    {
+        if(!empty($this->class_plural)) {
+            return $this->class_plural;
+        }
+        return Str::pluralStudly(class_basename($this));
+    }
+
+    public function getClassSlug($plural = false): string
+    {
+        $class_basename = class_basename($this);
+        if ($plural) {
+            $class_basename = $this->getClassPlural();
+        }
+
+        return Str::snake($class_basename);
     }
 
     public function getPermissionPrefix(): string
@@ -16,21 +79,27 @@ trait BaseModelTrait
         return $this->getClassSlug(true);
     }
 
-    public function getClassSlug($plural = false): string
-    {
-        if ($plural) {
-            return Str::snake(Str::pluralStudly(class_basename($this)));
-        }
-
-        return Str::snake(class_basename($this));
-    }
-
+    /**
+     * Static method to get class slug
+     * $entity::classSlug()
+     */
     public static function classSlug($plural = false): string
     {
+        $class_basename = class_basename(static::class);
         if ($plural) {
-            return Str::snake(Str::pluralStudly(class_basename(static::class)));
+            $class_basename = Str::pluralStudly($class_basename);
         }
+        return Str::snake($class_basename);
+    }
 
-        return Str::snake(class_basename(static::class));
+    /**
+     * DEPRECATED
+     *
+     * Static method to get table name
+     * $entity::getTableName()
+     */
+    public static function getTableName(): string
+    {
+        return with(new static)->getTable();
     }
 }
