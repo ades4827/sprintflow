7002df8a844f0d809aa0f6582f0016386cee033b
Michele Lucchina <michele.lucchina@jokersrl.it>
2025-10-16 17:23:16 +0200

    component action button

---
+++ resources/views/components/action-btn.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'action' => 'submit', // azione Livewire da lanciare
+    'label' => __('sprintflow::view.save'),    // testo del bottone
+    'icon' => 'fa-solid fa-floppy-disk', // icona di default
+    'icon_loading' => 'fa-solid fa-spinner fa-spin-pulse', // icona di default
+    'class' => 'btn btn-primary', // classe CSS base
+])
+<button
+    wire:click.prevent="{{ $action }}"
+    class="{{ $class }}"
+    wire:loading.attr="disabled"
+>
+    <div class="block sm:hidden">
+        <span wire:loading.remove>@if($icon)<i class="{{ $icon }}"></i>@endif{{ $label }}</span>
+        @if($icon_loading)
+            <i wire:loading class="{{ $icon_loading }}"></i>
+        @endif
+    </div>
+    <div class="hidden sm:block">
+		@if($icon)<i class="{{ $icon }} mr-1"></i>@endif
+        <span wire:loading.remove>{{ $label }}</span>
+        <span wire:loading>{{ __('sprintflow::view.wait') }}...</span>
+    </div>
+</button>
--- resources/views/livewire/form-footer.blade.php
+++ resources/views/livewire/form-footer.blade.php
@@ -1,57 +1,51 @@
 <div class="@if(isset($class)) {{ $class }} @else mt-5 @endif">
-    @if($errors->any())
-        <div class="pb-5">
-            <strong class="block text-danger">{{ __('sprintflow::view.attention') }}:</strong>
-            @foreach ($errors->all() as $error)
-                <small class="block text-danger">{{ $error }}</small>
-            @endforeach
-        </div>
-    @endif
-    <div class="grid grid-cols-3 gap-x-4 gap-y-2">
-        <div>
-            @if(method_exists($this, 'removeItem') && $entity)
-                <a wire:click="confirm('removeItem')" wire:loading.attr="disabled" class="btn btn-danger mr-1">
-                    <i class="fa-solid fa-trash sm:mr-1"></i>
-                    <span class="hidden sm:block">{{ __('sprintflow::view.delete') }}</span>
-                </a>
-            @endif
-        </div>
-        <div class="col-span-2 text-right">
-            @if(isset($this->is_modal) && $this->is_modal)
-                <a wire:click="$dispatch('closeModal')" wire:loading.attr="disabled" class="btn btn-outline-secondary mr-1">
-                    <i class="fa-solid fa-rotate-left sm:mr-1"></i>
-                    <span class="hidden sm:block">{{ __('sprintflow::view.cancel') }}</span>
-                </a>
-            @elseif( isset($form_cancel_route) )
-                <a href="{{ route($form_cancel_route) }}" wire:loading.attr="disabled" class="btn btn-outline-secondary mr-1">
-                    <i class="fa-solid fa-rotate-left sm:mr-1"></i>
-                    <span class="hidden sm:block">{{ __('sprintflow::view.cancel') }}</span>
-                </a>
-            @elseif( isset($form_cancel_url) )
-                <a href="{{ $form_cancel_url }}" wire:loading.attr="disabled" class="btn btn-outline-secondary mr-1">
-                    <i class="fa-solid fa-rotate-left sm:mr-1"></i>
-                    <span class="hidden sm:block">{{ __('sprintflow::view.cancel') }}</span>
-                </a>
-            @endif
-            @if( isset($save_and_edit) )
-                <a wire:click="submit('edit')" class="btn btn-primary hidden sm:inline-flex" wire:loading.attr="disabled">
-                    <span wire:loading.remove>{{ __('sprintflow::view.save_and_edit') }}</span>
-                    <span wire:loading>{{ __('sprintflow::view.wait') }}...</span>
-                </a>
-            @endif
-            @if( !isset($hide_submit) )
-                <button type="submit" class="btn btn-primary" wire:loading.attr="disabled">
-                    <div class="block sm:hidden">
-                        <span wire:loading.remove>@if(!isset($submit_label))<i class="fa-solid fa-floppy-disk"></i>@endif {{ isset($submit_label) ? $submit_label : __('sprintflow::view.save') }}</span>
-                        <i wire:loading class="fa-solid fa-spinner fa-spin-pulse"></i>
-                    </div>
-                    <div class="hidden sm:block">
-                        @if(!isset($submit_label))<i class="fa-solid fa-floppy-disk mr-1"></i>@endif
-                        <span wire:loading.remove>{{ isset($submit_label) ? $submit_label : __('sprintflow::view.save') }}</span>
-                        <span wire:loading>{{ __('sprintflow::view.wait') }}...</span>
-                    </div>
-                </button>
-            @endif
-        </div>
-    </div>
+	@if($errors->any())
+		<div class="pb-5">
+			<strong class="block text-danger">{{ __('sprintflow::view.attention') }}:</strong>
+			@foreach ($errors->all() as $error)
+				<small class="block text-danger">{{ $error }}</small>
+			@endforeach
+		</div>
+	@endif
+	<div class="grid grid-cols-3 gap-x-4 gap-y-2">
+		<div>
+			@if(method_exists($this, 'removeItem') && $entity)
+				<a wire:click="confirm('removeItem')" wire:loading.attr="disabled" class="btn btn-danger mr-1">
+					<i class="fa-solid fa-trash sm:mr-1"></i>
+					<span class="hidden sm:block">{{ __('sprintflow::view.delete') }}</span>
+				</a>
+			@endif
+		</div>
+		<div class="col-span-2 text-right">
+			@if(isset($this->is_modal) && $this->is_modal)
+				<a wire:click="$dispatch('closeModal')" wire:loading.attr="disabled" class="btn btn-outline-secondary mr-1">
+					<i class="fa-solid fa-rotate-left sm:mr-1"></i>
+					<span class="hidden sm:block">{{ __('sprintflow::view.cancel') }}</span>
+				</a>
+			@elseif( isset($form_cancel_route) )
+				<a href="{{ route($form_cancel_route) }}" wire:loading.attr="disabled" class="btn btn-outline-secondary mr-1">
+					<i class="fa-solid fa-rotate-left sm:mr-1"></i>
+					<span class="hidden sm:block">{{ __('sprintflow::view.cancel') }}</span>
+				</a>
+			@elseif( isset($form_cancel_url) )
+				<a href="{{ $form_cancel_url }}" wire:loading.attr="disabled" class="btn btn-outline-secondary mr-1">
+					<i class="fa-solid fa-rotate-left sm:mr-1"></i>
+					<span class="hidden sm:block">{{ __('sprintflow::view.cancel') }}</span>
+				</a>
+			@endif
+			@if( isset($save_and_edit) )
+				<a wire:click="submit('edit')" class="btn btn-primary hidden sm:inline-flex" wire:loading.attr="disabled">
+					<span wire:loading.remove>{{ __('sprintflow::view.save_and_edit') }}</span>
+					<span wire:loading>{{ __('sprintflow::view.wait') }}...</span>
+				</a>
+			@endif
+			@if( !isset($hide_submit) )
+				<x-sprintflow::action-btn
+						action="create"
+						:label="isset($submit_label) ? $submit_label : __('sprintflow::view.save')"
+						:icon="isset($submit_label) ? '' : 'fa-solid fa-floppy-disk'"
+				/>
+			@endif
+		</div>
+	</div>
 </div>
