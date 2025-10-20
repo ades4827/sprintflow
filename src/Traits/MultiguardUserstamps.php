f30774650f2d4175a64b979bb7d75af93e631378
Michele Lucchina <michele.lucchina@jokersrl.it>
2025-10-20 12:21:37 +0200

    Multiguard Userstamps

---
+++ src/Traits/MultiguardUserstamps.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ades4827\Sprintflow\Traits;
+
+trait MultiguardUserstamps
+{
+    use \Mattiverse\Userstamps\Traits\Userstamps;
+
+    protected function getUserClass(): string
+    {
+        foreach (config('auth.guards') as $guard_name => $guard) {
+            if (auth($guard_name)->check()) {
+                return config('auth.providers.'.$guard['provider'].'.model', config('auth.providers.users.model'));
+            }
+        }
+    }
+}
