b6b405779e76fe4e2c5db42b5b1585f0c736c2e9
Michele Lucchina <michele.lucchina@jokersrl.it>
2025-10-23 16:20:39 +0200

    action btn disabled

--- resources/views/components/action-btn.blade.php
+++ resources/views/components/action-btn.blade.php
@@ -4,11 +4,13 @@
     'icon' => 'fa-solid fa-floppy-disk', // icona di default
     'icon_loading' => 'fa-solid fa-spinner fa-spin-pulse', // icona di default
     'class' => 'btn btn-primary', // classe CSS base
+    'disabled' => false,
 ])
 <button
     wire:click.prevent="{{ $action }}"
     class="{{ $class }}"
     wire:loading.attr="disabled"
+    @if($disabled) disabled="disabled" @endif
 >
     <div class="block sm:hidden">
         <span wire:loading.remove>@if($icon)<i class="{{ $icon }}"></i>@endif{{ $label }}</span>
