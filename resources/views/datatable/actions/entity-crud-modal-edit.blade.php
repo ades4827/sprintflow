c3283c499235826e0cf9dea40e027ec137b7f644
Michele Lucchina <michele.lucchina@jokersrl.it>
2026-01-12 11:34:40 +0100

    fix: better datatable modal edit

--- resources/views/datatable/actions/entity-crud-modal-edit.blade.php
+++ resources/views/datatable/actions/entity-crud-modal-edit.blade.php
@@ -4,12 +4,12 @@
 			<li>
 				<a onclick='Livewire.dispatch("openModal", {
                         component: "modal-edit", arguments: {
-                            "load_entity": true,
-                            "model_class": {{ json_encode(get_class($model)) }},
-                            "model": "{{ $model->getClassSlug() }}",
-                            "model_id": {{ $model->id }},
-                            "modal_title": "{{ isset($modal_title) ? $modal_title : '' }}",
-                            "component_folder": "{{ isset($component_folder) ? $component_folder : '' }}",
+                            load_entity: true,
+                            model_class: {{ json_encode(get_class($model)) }},
+                            model: "{{ $model->getClassSlug() }}",
+                            model_id: {{ $model->id }},
+                            modal_title: "{{ isset($modal_title) ? $modal_title : '' }}",
+                            component_folder: "{{ isset($component_folder) ? $component_folder : null }}",
                         }
                     })' class="btn btn-primary btn-icon-sm tooltip"
 				   data-placement="bottom" title="{{ __('sprintflow::view.edit') }}">
