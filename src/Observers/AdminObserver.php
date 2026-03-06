be493ca9c1ec88b383a9d5651b8f5d57d6737737
Michele Lucchina <michele.lucchina@jokersrl.it>
2026-03-06 18:10:16 +0100

    fix: observer

--- src/Observers/AdminObserver.php
+++ src/Observers/AdminObserver.php
@@ -8,54 +8,28 @@ use Illuminate\Support\Facades\Cache;
 
 class AdminObserver
 {
-    /**
-     * Handle the Admin "created" event.
-     *
-     * @param  Admin  $admin
-     * @return void
-     */
-    public function created(Admin $admin)
+    public function creating(Admin $admin)
     {
-        $admin->update([
-            'complete_name' => $admin->name_formatted,
-        ]);
+        $admin->complete_name = $admin->name_formatted;
     }
 
-    /**
-     * Handle the Admin "updated" event.
-     *
-     * @param  Admin  $admin
-     * @return void
-     */
+    public function updating(Admin $admin)
+    {
+        $admin->complete_name = $admin->name_formatted;
+    }
+
     public function updated(Admin $admin)
     {
         Cache::forget('admins_'.$admin->id);
-
-        if ($admin->complete_name != $admin->name_formatted) {
-            $admin->complete_name = $admin->name_formatted;
-            $admin->save();
-        }
     }
 
-    /**
-     * Handle the Admin "deleted" event.
-     *
-     * @param  Admin  $admin
-     * @return void
-     */
     public function deleted(Admin $admin)
     {
         $admin->update([
-            'email' => time().User::DELETE_TOKEN.$admin->email,
+            'email' => time(). User::DELETE_TOKEN.$admin->email,
         ]);
     }
 
-    /**
-     * Handle the Admin "restored" event.
-     *
-     * @param  Admin  $admin
-     * @return void
-     */
     public function restored(Admin $admin)
     {
         $email = explode(User::DELETE_TOKEN, $admin->email);
@@ -64,12 +38,6 @@ class AdminObserver
         ]);
     }
 
-    /**
-     * Handle the Admin "force deleted" event.
-     *
-     * @param  Admin  $admin
-     * @return void
-     */
     public function forceDeleted(Admin $admin)
     {
         //
