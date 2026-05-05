c47ebb60ccd7109c977803869cc0b43342439261
Michele Lucchina <michele.lucchina@jokersrl.it>
2026-05-05 10:22:43 +0200

    fix: MultiguardUserstamps get by guard name

--- src/Traits/MultiguardUserstamps.php
+++ src/Traits/MultiguardUserstamps.php
@@ -6,8 +6,15 @@ trait MultiguardUserstamps
 {
     use \Mattiverse\Userstamps\Traits\Userstamps;
 
-    protected function getUserClass(): string
+    protected function getUserClass(?string $by_guard_name): string
     {
+        if($by_guard_name) {
+            foreach (config('auth.guards') as $guard_name => $guard) {
+                if ($guard_name === $by_guard_name) {
+                    return config('auth.providers.'.$guard['provider'].'.model', config('auth.providers.users.model'));
+                }
+            }
+        }
         foreach (config('auth.guards') as $guard_name => $guard) {
             if (auth($guard_name)->check()) {
                 return config('auth.providers.'.$guard['provider'].'.model', config('auth.providers.users.model'));
