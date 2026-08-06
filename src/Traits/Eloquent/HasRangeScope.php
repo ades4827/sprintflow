de4f8bb8911cf73417e085227c3a082a51ff6133
Michele Lucchina <michele.lucchina@jokersrl.it>
2026-08-06 09:48:59 +0200

    feature: HasRangeScope dynamic label

--- src/Traits/Eloquent/HasRangeScope.php
+++ src/Traits/Eloquent/HasRangeScope.php
@@ -9,9 +9,9 @@ trait HasRangeScope
     /**
      * Scope a query for date range
      *
-     * use: Model::whereRange($start, $end)->get();
+     * use: Model::whereRange(start: $start, end: $end, inclusive: false, start_field: 'from_date', end_field:'to_date')->get();
      */
-    public function scopeWhereRange($query, $start, $end, $inclusive = true)
+    public function scopeWhereRange($query, $start, $end, $inclusive = true, $start_field = 'from', $end_field = 'to')
     {
         $start = $start instanceof Carbon ? $start : Carbon::parse($start);
         $end = $end instanceof Carbon ? $end : Carbon::parse($end);
@@ -21,12 +21,12 @@ trait HasRangeScope
             $end = $end->clone()->subMinute(1);
         }
 
-        return $query->where(function ($query) use ($start, $end) {
-            $query->whereBetween('from', [$start, $end])
-                ->orWhereBetween('to', [$start, $end])
-                ->orWhere(function ($query) use ($start, $end) {
-                    $query->where('from', '<=', $start)
-                        ->where('to', '>=', $end);
+        return $query->where(function ($query) use ($start, $end, $start_field, $end_field) {
+            $query->whereBetween($start_field, [$start, $end])
+                ->orWhereBetween($end_field, [$start, $end])
+                ->orWhere(function ($query) use ($start, $end, $start_field, $end_field) {
+                    $query->where($start_field, '<=', $start)
+                        ->where($end_field, '>=', $end);
                 });
         });
     }
